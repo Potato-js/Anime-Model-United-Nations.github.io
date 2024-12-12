36a8461e9a3c1472eeea407e3218bebf5fe3cafd
gehdron <zenggordon9@gmail.com>
2024-12-11 19:13:43 -0500

    Started work on main.html

--- src/main.js
+++ src/main.js
@@ -1,10 +1,15 @@
 const hamburger = document.querySelector(".nav__hamburger");
 const linksContainer = document.querySelector(".nav__menu");
 const links = document.querySelector(".nav__menu__link");
+const navbar = document.querySelector(".nav");
 
 hamburger.addEventListener("click", () => {
     linksContainer.classList.toggle("active");
     hamburger.classList.toggle("active");
+});
+
+navbar.onload(() =>{
+    navbar.toggle("active");
 });
 
 window.addEventListener("resize", () =>{
@@ -24,4 +29,14 @@ function closeMenu(){
             hamburger.classList.remove("active");
         });
     });
+}
+var prevScrollpos = window.scrollY;
+window.onscroll = function() {
+  var currentScrollPos = window.scroll;
+  if (prevScrollpos > currentScrollPos) {
+    navbar.style.top = "0";
+  } else {
+    navbar.style.top = "-px";
+  }
+  prevScrollpos = currentScrollPos;
 }
